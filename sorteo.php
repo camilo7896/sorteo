39641c16d1898acafb856fa541d7a2470d50e928
camilo7896 <63164154+camilo7896@users.noreply.github.com>
2020-08-06 23:34:17 -0500

    Add files via upload

---
+++ sorteo.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>El suertudo</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+
+</head>
+<body>
+    <div class="container">
+            <header class="titulo">
+                    <h1> <div class="spam"> l  f q k o </div> El suertudo <div class="spam"> m n a f  l </div> </h1>
+                       
+            </header>
+            <article id="boxacomulado"> 
+            <h3>acomulado</h3>
+        <center>    <div class="resultado"></div></center>
+        </article>
+            <!--**********menu**********-->
+     <nav>
+        <center>     <ul>
+                 <a href="#"><li class="btn btn-warning" id="boton">Ganadores</li></a>   
+                   <a href="#"> <li class="btn btn-warning" id="boton">Lanzamiento del sorteo</li></a>
+                    
+                </ul></center>
+            </nav>
+                <!--***********fin del menu***************-->
+                <!--*************descrippcion del juego******************-->
+            
+            <article>
+                <h3> El suertudo</h3>
+                <h3 class="p">
+                    El suertudo es un juego de suerte donde los jugadores tendran que comprar un tiquet, el cual el programa arrojara una serie de numeros 
+                    aleatorios con el cual podran ganar la cifra acomulada durante los 5 dias. 
+                    <br>en caso de que ningunos de los participantes gane la 
+                    cifra, quedara acomulada para el proximo sorteo.
+                </h3>
+        </article>
+        <article>
+            <h3>ganadores</h3>
+            <h3 class="p">Los ganadores recibiran el 80% del monto acomulado durante los 5 dias
+                <br>
+                el dinero podra ser retirado en el lugar donde se compro el tiquet.
+            </h3>
+        </article>
+        <!--*******************fin de descripcion del juego*******************************-->
+        
+ <!--******************************social***************************************-->
+           
+        <a href="#"><button type="button" class="btn btn-primary">Facebook</button></a>
+           
+
+
+       </div>
+
+    </div>
+    
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+</body>
+</html>
